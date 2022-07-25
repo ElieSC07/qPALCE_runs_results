98762254d3798b0e24ea26f792e75d60b564c5a0
Elijah Cishugi <48275230+ElieSC07@users.noreply.github.com>
2022-07-25 21:31:50 +0200

    counter

---
+++ counter4_directory/counter4.v
@@ -0,0 +1,11 @@
+module counter (clk, rst, en, count);
+
+    input clk, rst, en;
+    output reg [3:0] count;
+
+    always @(posedge clk)
+        if(rst)
+            count <= 3'd0;
+        else if (en)
+            count <= count + 3'd1;
+endmodule
