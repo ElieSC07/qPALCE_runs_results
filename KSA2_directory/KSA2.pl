f3dde7b355006ea3a3e148935a090df4c79c15c7
Elijah Cishugi <48275230+ElieSC07@users.noreply.github.com>
2022-07-25 11:13:00 +0200

    KSA2

---
+++ KSA2_directory/KSA2.pl
@@ -0,0 +1,45 @@
+UCLA pl 1.0
+# Written by BLIF2BOOKSHELF on Mon Jul 25 11:10:38 2022
+# SPORT Lab, University of Southern California, Los Angeles, CA 90089
+# Developers: Ting-Ru Lin <tingruli@usc.edu> and Massoud Pedram <pedram@usc.edu>
+
+	XOR2T_10_n10	0	0	: N
+	XOR2T_11_n11	0	0	: N
+	XOR2T_15_sum1	0	0	: N
+	XOR2T_9_n9	0	0	: N
+	AND2T_12_n12	0	0	: N
+	AND2T_13_n13	0	0	: N
+	AND2T_16_n16	0	0	: N
+	AND2T_17_n17	0	0	: N
+	AND2T_18_n18	0	0	: N
+	DFFT_21__FPB_n48	0	0	: N
+	DFFT_22__FPB_n49	0	0	: N
+	DFFT_23__FPB_n50	0	0	: N
+	DFFT_24__FPB_n51	0	0	: N
+	DFFT_25__FPB_n52	0	0	: N
+	DFFT_26__FPB_n53	0	0	: N
+	DFFT_27__FPB_n54	0	0	: N
+	OR2T_14_n14	0	0	: N
+	OR2T_19_n19	0	0	: N
+	DFFT_28__FPB_n55	0	0	: N
+	DFFT_29_sum0	0	0	: N
+	OR2T_20_cout	0	0	: N
+	Split_30_n57	0	0	: N
+	Split_31_n58	0	0	: N
+	Split_32_n59	0	0	: N
+	Split_33_n60	0	0	: N
+	Split_34_n61	0	0	: N
+	Split_35_n62	0	0	: N
+	Split_36_n63	0	0	: N
+	Split_37_n64	0	0	: N
+	Split_38_n65	0	0	: N
+	Split_39_n66	0	0	: N
+	a0_Pad	625	1020	: /FIXED
+	a1_Pad	1150	605	: /FIXED
+	b0_Pad	625	280	: /FIXED
+	b1_Pad	300	570	: /FIXED
+	cin_Pad	875	1020	: /FIXED
+	GCLK_Pad	300	850	: /FIXED
+	cout_Pad	300	710	: /FIXED
+	sum0_Pad	1150	815	: /FIXED
+	sum1_Pad	875	280	: /FIXED
