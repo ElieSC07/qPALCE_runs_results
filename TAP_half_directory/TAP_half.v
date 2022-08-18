a339c8c4d75e51b3ce773fe404dc8788f7a2af48
Elijah Cishugi <48275230+ElieSC07@users.noreply.github.com>
2022-08-18 16:30:30 +0200

    r

--- TAP_half_directory/TAP_half.v
+++ TAP_half_directory/TAP_half.v
@@ -1,173 +1,85 @@
-// No implementation of the actual output logic of the TAP controller, yet
-
-module TAP_half(TMS, TRST, clk, state_obs0, state_obs1, state_obs2, state_obs3); 
-    input TMS, TRST, clk; // clk :: TCK
-    output state_obs0, state_obs1, state_obs2, state_obs3; 
-    reg state_obs0, state_obs1, state_obs2, state_obs3;
-    reg[3:0] state_obs;
-    reg[3:0] state; 
+module state_machine(clk, TRST, TMS, output1, state_obs0, state_obs1); 
+    input clk, TRST, TMS; 
+    output output1, state_obs0, state_obs1; 
+    reg output1, state_obs0, state_obs1; 
+    reg[1:0] state; 
 
         /* Make State Assignments */
-    parameter [3:0] Test_logic_Reset = 4'b0000, Run_Test_Idle = 4'b0001,
-                    Select_IR_Scan = 4'b0010, Capture_IR = 4'b0011, Shift_IR = 4'b0100, Exit1_IR = 4'b0101, Pause_IR = 4'b0110, Exit2_IR = 4'b0111, Update_IR = 4'b1000;
-
-    initial begin
-        state <= Test_logic_Reset;
-        state_obs <= 4'b0000;   
-        state_obs0 <= 0;
-        state_obs1 <= 0;
-        state_obs2 <= 0;
-        state_obs3 <= 0;
-    end
-
-//    always @(posedge clk or posedge TRST)
-    always @(posedge clk)
+    parameter [1:0] Test_logic_Reset = 0, Run_Test_Idle = 1, Select_DR_Scan = 2; 
+    always @(posedge clk or posedge TRST)
         begin 
             if (TRST==1) begin
-                state <= Test_logic_Reset;
+                state <= Test_logic_Reset; 
+                output1    <= 0;
+                state_obs0 <= 0;
+                state_obs1 <= 0;
             end
-
-            else if (TRST==0) begin
+            else 
                 /* Define the next state transitions using a case statement based on the current state */ 
                 case (state)
                     Test_logic_Reset: begin
-                        if (TMS == 1) begin
-                            state <= Test_logic_Reset;
-                        end
-                        else begin
-                            state <= Run_Test_Idle; 
-                        end
-                    end
-
-                    Run_Test_Idle: begin
-                        if (TMS == 1) begin
-                            state <= Select_IR_Scan; 
-                        end
-                        else begin
-                            state <= Run_Test_Idle; 
-                        end
-                    end    
-
-                    Select_IR_Scan: begin
-                        if (TMS == 1) begin
+                        if (TMS==1) begin
                             state <= Test_logic_Reset; 
+                            output1    <= 0; 
+                            state_obs0 <= 0;
+                            state_obs1 <= 0;
                         end
-                        else begin
-                            state <= Capture_IR; 
-                        end
-                    end 
-
-                    Capture_IR: begin
-                        if (TMS == 1) begin
-                            state <= Exit1_IR; 
-                        end
-                        else begin
-                            state <= Shift_IR; 
-                        end
-                    end 
-
-                    Shift_IR: begin
-                        if (TMS == 1) begin
-                            state <= Exit1_IR; 
-                        end
-                        else begin 
-                            state <= Shift_IR;
-                        end
-                    end
-
-                    Exit1_IR: begin
-                        if (TMS == 1) begin
-                            state <= Update_IR; 
-                        end
-                        else begin
-                            state <= Pause_IR; 
-                        end
-                    end  
-                    
-                    Pause_IR: begin
-                        if (TMS == 1) begin
-                            state <= Exit2_IR; 
-                        end
-                        else begin
-                            state <= Pause_IR; 
-                        end
-                    end   
-
-                    Exit2_IR: begin
-                        if (TMS == 1) begin
-                            state <= Update_IR; 
-                        end
-                        else begin
-                            state <= Shift_IR; 
-                        end
-                    end 
-
-                    Update_IR: begin
-                        if (TMS == 1) begin
-                            state <= Select_IR_Scan; 
-                        end
-                        else begin
+                        else if (TMS==0) begin
                             state <= Run_Test_Idle; 
+                            output1    <= 0;
+                            state_obs0 <= 1;
+                            state_obs1 <= 0;
                         end
                     end
-
-                    default: begin
-                        state <= Test_logic_Reset;
-                    end 
-                endcase
-            end
-        end
-
-    always @(posedge clk)
-        begin
-            case(state)
-                    Test_logic_Reset: begin
-                        state_obs <= 4'b0000;
-                    end 
-
                     Run_Test_Idle: begin
-                        state_obs <= 4'b0001;
-                    end 
-
-                    Select_IR_Scan: begin
-                        state_obs <= 4'b0010;
-                    end 
-
-                    Capture_IR: begin
-                        state_obs <= 4'b0011;
-                    end 
-
-                    Shift_IR: begin
-                        state_obs <= 4'b0100;
-                    end 
-
-                    Exit1_IR: begin
-                        state_obs <= 4'b0101;
-                    end 
-
-                    Pause_IR: begin
-                        state_obs <= 4'b0110;
-                    end 
-
-                    Exit2_IR: begin
-                        state_obs <= 4'b0111;
-                    end 
-
-                    Update_IR: begin
-                        state_obs <= 4'b1000;
-                    end 
-                    default: begin
-                        state_obs <= 4'b0000;  
+                        if (TMS==1) begin
+                            state <= Select_DR_Scan; 
+                            output1    <= 1;
+                            state_obs0 <= 0;
+                            state_obs1 <= 1;
+                        end
                     end
-            endcase
-        end
+                    Select_DR_Scan: begin
+                        if (TMS==1) begin
+                            state <= Test_logic_Reset; 
+                            output1    <= 0;
+                            state_obs0 <= 0;
+                            state_obs1 <= 0;
+                        end
+                    end
+                    default: begin 
+                        state <= Test_logic_Reset; 
+                        output1    <= 0;
+                        state_obs0 <= 0;
+                        state_obs1 <= 0;
+                    end
+                endcase
+        end 
 
-    always @(posedge clk)
+        /* Define State Machine Outputs */
+/*    always @(state)
         begin
-        state_obs0 <= state_obs[0];
-        state_obs1 <= state_obs[1];
-        state_obs2 <= state_obs[2];
-        state_obs3 <= state_obs[3];
-    end
-
-endmodule
+            case(state)  
+                state_A: begin
+                    output1    <= 0;
+                    state_obs1 <= 0;
+                    state_obs0 <= 0;
+                end
+                state_B: begin
+                    output1    <= 1;
+                    state_obs1 <= 0;
+                    state_obs0 <= 1;
+                end
+                state_C: begin
+                    output1    <= 0; 
+                    state_obs1 <= 1;
+                    state_obs0 <= 0;
+                end
+                default: begin
+                    output1    <= 0; 
+                    state_obs1 <= 0;
+                    state_obs0 <= 0;
+                end
+            endcase
+        end */
+endmodule 
